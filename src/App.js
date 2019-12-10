fa5a0412bf5593d272cd9455b9dc792d29594bba
Andy Brackley <andybrackley@gmail.com>
2019-12-10 14:36:37 +0000

    Testing out child component with Props and State

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class TestComponent extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { text: props.message, counter: 1 }
+  }
+
+  render() {
+    return ( 
+      <div>Text={this.state.text} counter={this.state.counter}</div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TestComponent message={"Test Message"}></TestComponent>
       </header>
     </div>
   );
